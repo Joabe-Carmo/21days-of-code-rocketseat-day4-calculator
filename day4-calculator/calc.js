4e92b3fc54a078a30b646079b823b3e750f6cde2
Joabe Carmo <107870276+Joabe-Carmo@users.noreply.github.com>
2022-08-25 13:09:35 -0300

    Add files via upload

---
+++ day4-calculator/calc.js
@@ -0,0 +1,17 @@
+var n1 = document.querySelector('#n1')
+var n2 = document.querySelector('#n2')
+
+let result = document.querySelector('span')
+
+function addition(){
+  result.innerHTML = parseFloat(n1.value) + parseFloat(n2.value) 
+}
+function subtraction(){
+  result.innerHTML = parseFloat(n1.value) + parseFloat(n2.value) 
+}
+function multiplication(){
+  result.innerHTML = parseFloat(n1.value) + parseFloat(n2.value) 
+}
+function division(){
+  result.innerHTML = parseFloat(n1.value) + parseFloat(n2.value) 
+}
